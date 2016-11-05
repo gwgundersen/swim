20d8b5e9b9ece3a82ec6d4db49e2860beac6417c
Greg <ggundersen@gmail.com>
2016-11-05 12:49:41 -0400

    Double to single quotes.

--- swim/__init__.py
+++ swim/__init__.py
@@ -16,17 +16,17 @@ db = SQLAlchemy()
 from swim import models
 
 app = Flask(__name__,
-            static_url_path="%s/static" % config.get("url", "base"),
-            static_folder="static")
+            static_url_path='%s/static' % config.get('url', 'base'),
+            static_folder='static')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://%s:%s@%s:3306/%s" % (
-    config.get("db", "user"),
-    config.get("db", "passwd"),
-    config.get("db", "host"),
-    config.get("db", "db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:3306/%s' % (
+    config.get('db', 'user'),
+    config.get('db', 'passwd'),
+    config.get('db', 'host'),
+    config.get('db', 'db')
 )
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 1800  # Recycle every 30 min.
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 1800  # Recycle every 30 min.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 with app.app_context():
@@ -36,15 +36,15 @@ with app.app_context():
 
 """ URL configuration
     ------------------------------------------------------------------------"""
-app.config.base_tag_url = "/swim/"
+app.config.base_tag_url = '/swim/'
 
 
 """ Debugging
     ------------------------------------------------------------------------"""
 # It's impossible to debug a 500 error in production without seeing an error
 # message.
-app.config["DEBUG"] = True
-app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config['DEBUG'] = True
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 
 """ Server endpoints
@@ -58,11 +58,11 @@ app.register_blueprint(endpoints.task_blueprint)
 """ Login session management
     ------------------------------------------------------------------------"""
 # Change this key to force all users to re-authenticate.
-app.secret_key = config.get("cookies", "secret_key")
+app.secret_key = config.get('cookies', 'secret_key')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "auth.login"
+login_manager.login_view = 'auth.login'
 
 
 @app.before_request
